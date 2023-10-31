e01f1f05e08393ad4c5b9767af7f5987f42983d5
KeddyB <bokelusi@gmail.com>
2023-10-31 12:44:07 +0100

    commit

--- frontend/src/components/CreatePin.jsx
+++ frontend/src/components/CreatePin.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AiOutlineCloudUpload } from 'react-icons/ai'
+import { MdDelete } from 'react-icons/md'
+import { useNavigate } from 'react-router-dom'
 
-const CreatePin = () => {
+import { client } from '../client'
+import Spinner from './Spinner'
+import { categories } from '../utils/data'
+
+const CreatePin = ({ user }) => {
+  const [title, setTitle] = useState('');
+  const [destination, setDestination] = useState('');
+  const [about, setAbout] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [fields, setFields] = useState(null);
+  const [categories, setCategories] = useState(null);
+  const [imageAsset, setImageAssets] = useState(null);
+  const [wrongImageType, setWrongImageType] = useState(false);
+
+  const navigate = useNavigate();
+
   return (
     <div>
-      CreatePin
+      
     </div>
   )
 }
