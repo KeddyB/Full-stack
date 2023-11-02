cd002963963bc64b825ac97d28892779835f43ed
KeddyB <bokelusi@gmail.com>
2023-11-02 12:46:05 +0100

    first commit of the day

--- frontend/src/components/CreatePin.jsx
+++ frontend/src/components/CreatePin.jsx
@@ -101,6 +101,7 @@ const CreatePin = ({ user }) => {
             type="text" 
             className='outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2' 
             value={about}
+            onChange={(e) => setAbout(e.target.value)}
             placeholder='What is your Pin about?'
           />
           <input 
