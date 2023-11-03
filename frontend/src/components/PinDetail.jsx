9309d7c34e750c5f6e870e1f608a2dd35f6cf6f6
KeddyB <bokelusi@gmail.com>
2023-11-03 12:12:51 +0100

    this is a commit

--- frontend/src/components/PinDetail.jsx
+++ frontend/src/components/PinDetail.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { MdDownloadForOffline } from 'react-icons/md'
+import { Link, useParams } from 'react-router-dom'
+import { v4 as uuidv4 } from 'uuid'
 
-const PinDetail = () => {
+import { client, urlFor } from '../client'
+import MasonryLayout from './MasonryLayout'
+import { pinDetailMorePinQuery, pinDetailQuery } from '../utils/data'
+import Spinner from './Spinner'
+
+const PinDetail = ({ user }) => {
   return (
     <div>
-      PinDetail
+      this is a pin details
     </div>
   )
 }
