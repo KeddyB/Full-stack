c2ecbd3b2b5457d844a93eb30defae424310e26a
KeddyB <bokelusi@gmail.com>
2023-11-06 13:49:04 +0100

    commit

--- frontend/src/components/MasonryLayout.jsx
+++ frontend/src/components/MasonryLayout.jsx
@@ -4,11 +4,13 @@ import Pin from './Pin'
 
 const breakPointObj = {
   default: 4,
-  3000: 6,
-  2000: 5,
-  1200: 3,
-  1000: 2,
-  500: 1,
+  3000: 8,
+  2000: 7,
+  1500: 5,
+  1200: 4,
+  1000: 3,
+  700: 2,
+  500: 1
 }
 const MasonryLayout = ({ pins }) => {
   return (
