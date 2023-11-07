2e384564a2a400cce23684cc65342d8aba235021
KeddyB <bokelusi@gmail.com>
2023-11-07 15:06:37 +0100

    comment

--- frontend/src/components/PinDetail.jsx
+++ frontend/src/components/PinDetail.jsx
@@ -97,16 +97,16 @@ const PinDetail = ({ user }) => {
             </Link>
             <h2 className="mt-5 text-2xl">Comments</h2>
             <div className="max-h-370 overflow-y-auto">
-              {pinDetail?.comments?.map((item) => (
-                <div className="flex gap-2 mt-5 items-center bg-white rounded-lg" key={item.comment}>
+              {pinDetail?.comments?.map((comment) => (
+                <div className="flex gap-2 mt-5 items-center bg-white rounded-lg" key={comment.comment}>
                   <img
-                    src={item.postedBy?.image}
+                    src={comment.postedBy?.image}
                     className="w-10 h-10 rounded-full cursor-pointer"
                     alt="user-profile"
                   />
                   <div className="flex flex-col">
-                    <p className="font-bold">{item.postedBy?.userName}</p>
-                    <p>{item.comment}</p>
+                    <p className="font-bold">{comment.postedBy?.userName}</p>
+                    <p>{comment.comment}</p>
                   </div>
                 </div>
               ))}
