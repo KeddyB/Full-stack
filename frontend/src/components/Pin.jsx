239a8dbc589ff1be0fc11dad5a632e68f3e790cd
KeddyB <bokelusi@gmail.com>
2023-11-08 14:49:13 +0100

    done with pin details

--- frontend/src/components/Pin.jsx
+++ frontend/src/components/Pin.jsx
@@ -103,7 +103,7 @@ const Pin = ({ pin }) => {
                                 className='bg-white flex items-center gap-2 text-black font-bold p-2 rounded-full opacity-70 hover:opacity-100 hover:shadow-md'    
                             >
                                 <BsFillArrowUpRightCircleFill />
-                                {destination.length > 20 ? destination.slice(8, 17) : destination.slice(8)}
+                                {destination.length > 15 ? `${destination.slice(0, 15)}...` : destination}
                             </a>
                         )}
                         {postedBy?._id === user.googleId && (
