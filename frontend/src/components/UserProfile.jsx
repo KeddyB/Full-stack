09f541cb0df9e0be4da67d376615fa1b1d5707fa
KeddyB <bokelusi@gmail.com>
2023-11-08 15:37:01 +0100

    random image

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -3,11 +3,13 @@ import { useState, useEffect } from 'react'
 import { AiOutlineLogout } from 'react-icons/ai'
 import { useParams, useNavigate } from 'react-router-dom'
 import { GoogleLogout } from 'react-google-login'
+import { client } from '../client'
 
 import { userCreatedPinsQuery, userQuery, userSavedPinsQuery } from '../utils/data'
 import MasonryLayout from './MasonryLayout'
 import Spinner from './Spinner'
 
+const randomImage = 'https://source.unsplash.com/1600x900/?nature,photography,technology'
 const UserProfile = () => {
   const [user, setUser] = useState(null)
   const [pins, setPins] = useState(null)
@@ -15,14 +17,36 @@ const UserProfile = () => {
   const [activeBtn, setActiveBtn] = useState('created')
 
   const navigate = useNavigate()
-  const userId = useParams()
+  const { userId } = useParams()
+
+  useEffect(() => {
+    const query = userQuery(userId)
+
+    client
+      .fetch(query)
+      .then((data) => {
+        setUser(data[0])
+      })
+
+  }, [userId])
 
   if(!user){
     return <Spinner message={'Loading User profile.....'} />
   }
+
   return (
-    <div>
-      this is the user progfile
+    <div className='relative pb-2 h-full justify-center items-center'>
+      <div className="flex flex-col pb-5">
+        <div className="relative flex flex-col mb-7">
+          <div className="flex flex-col justify-center items-center">
+            <img
+              src={randomImage}
+              alt="banner-pic"
+              className='w-full h-370 2xl:h-510 shadow-lg object-cover' 
+            />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
