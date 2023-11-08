145303bfe367719adafd463b0ac4deee165dca8f
KeddyB <bokelusi@gmail.com>
2023-11-08 15:05:34 +0100

    commit

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { AiOutlineLogout } from 'react-icons/ai'
+import { useParams, useNavigate } from 'react-router-dom'
+import { GoogleLogout } from 'react-google-login'
+
+import { userCreatedPinsQuery, userQuery, userSavedPinsQuery } from '../utils/data'
+import MasonryLayout from './MasonryLayout'
+import Spinner from './Spinner'
 
 const UserProfile = () => {
+  const [user, setUser] = useState(null)
+  const [pins, setPins] = useState(null)
+  const [text, setText] = useState('Created')
+  const [activeBtn, setActiveBtn] = useState('created')
+
+  const navigate = useNavigate()
+  const userId = useParams()
+
+  if(!user){
+    return <Spinner message={'loading profile'} />
+  }
   return (
     <div>
-      UserProfile
+      this is the user progfile
     </div>
   )
 }
