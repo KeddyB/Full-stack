9faeac6a25e1d81b2fd7db66a6d3f31a3d3ce9ed
KeddyB <bokelusi@gmail.com>
2023-11-09 16:01:26 +0100

    commit

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,8 +3,6 @@ import GoogleLogin from 'react-google-login'
 import { useNavigate } from 'react-router-dom'
 import { FcGoogle } from 'react-icons/fc'
 import shareVideo from '../assets/share.mp4'
-import { gapi } from "gapi-script"
-import { useEffect } from "react"
 import logo from '../assets/logowhite.png'
 
 import { client } from '../client'
