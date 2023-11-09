43edc357673c8dfc4af476562e42e17988fbc1aa
KeddyB <bokelusi@gmail.com>
2023-11-09 16:08:30 +0100

    final commit

--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -1,7 +1,7 @@
 export const categories = [
   {
     name: 'memes',
-    image: 'https://photos.google.com/photo/AF1QipOUysb0XOz129Wq2l9HfXoEJ0urEJPYyI0Ap_67',
+    image: './utils/short.jpg',
   },
   {
     name: 'cars',
