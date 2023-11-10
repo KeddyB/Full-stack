ca393cb34a4ba8b19d855475633078fe7ac4b100
KeddyB <bokelusi@gmail.com>
2023-11-10 09:46:09 +0100

    commit

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
 
-const Search = () => {
+import MasonryLayout from './MasonryLayout'
+import { feedQuery, searchQuery } from '../utils/data'
+import Spinner from './Spinner'
+
+const Search = ({ searchTerm }) => {
+  const [pins, setPins] = useState(null)
+  const [loading, setLoading] = useState(false)
   return (
     <div>
-      search
+      {loading && <Spinner message={'searching for pins'}/>} 
     </div>
   )
 }
